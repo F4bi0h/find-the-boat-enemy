756a6e8477b8332394c31901457c2c60313de645
F4bi0h <fabiohn20@gmail.com>
2023-10-09 23:00:05 -0300

    update

--- js/game.js
+++ js/game.js
@@ -11,70 +11,117 @@ class Game {
     constructor() {
         this.timer = 30;
         this.areaGame = document.querySelector('.area-game');
-        this.boats = [
-            1,2,3,4,5,6,7,8,9,10,11,12,
-            13,14,15,16,17,18,19,20,21,22,
-            23,24,25,26,27,28,29,30,31,32,
-            33,34,35,36,37,38,39,40,41,42,
-            43,44,45,46,47,48,49,50,51,52,
-            53,54,55,56,57,58,59,60,61,62,63,64,
-            65,66,67,68,69,70,71,72,73,74,75,76,
-            77,78,79,80,81,82,83,84,85,86,
-            87,88,89,90,91
-        ];
+        this.boats = [];
+        this.enemy = Math.floor(Math.random() * 82);
 
-        this.loadGame();
+        this.showNamePlayer();
+        this.createBoat();
+        this.createRandomBoats();
     }
 
-    showInformations() {
+    showNamePlayer() {
         document.querySelector('.player').innerHTML = JSON.parse(localStorage.getItem('Player'));
     }
 
-    createBoats() {
-        this.boat = document.createElement('img');
-        this.boat.src = '/img/barco1.png';
-        this.boat.classList.add('boat');
+    createBoat() {
+        let boat = document.createElement('img');
+        boat.src = '/img/barco1.png';
+        boat.classList.add('boat');
 
-        document.querySelector('.area-game').append(this.boat);
-
-        this.boat.addEventListener('click', function() {
-            this.src = '/img/Explosion.png';
+        boat.addEventListener('click', () => {
+            boat.src = '/img/Explosion.png';
             let audio = document.querySelector('audio');
             audio.src = '/music/Som de explosão.mp3';
             audio.volume = 0.1;
             audio.play();
-        })
-    }
-
-    time() {
-        setInterval(() => {
-            this.timer--
-            document.querySelector('.timer').innerHTML = this.timer;
-            if(this.timer == 0) {
-                this.timer = 30;
-            }
-        }, 1000);
-    }
-
-    randomBoats() {
-        this.boats.forEach(b => {
-            this.createBoats();
         });
+
+        this.areaGame.append(boat);
     }
 
-    checkWin() {
-        
-    }
-
-    loadGame() {
-        this.showInformations();
-        this.randomBoats();
-        this.time();
-        this.checkWin();
+    createRandomBoats() {
+        for(let i = 0; i < 83; i++) {
+            this.boats.push(i);
+        }
+        console.log(this.enemy);
+        this.boats.forEach(b => {
+            this.createBoat();
+        });
     }
 }
 
 let game = new Game();
+
+
+
+// class Game {
+//     constructor() {
+//         this.timer = 30;
+//         this.areaGame = document.querySelector('.area-game');
+//         this.boats = [
+//             1,2,3,4,5,6,7,8,9,10,11,12,
+//             13,14,15,16,17,18,19,20,21,22,
+//             23,24,25,26,27,28,29,30,31,32,
+//             33,34,35,36,37,38,39,40,41,42,
+//             43,44,45,46,47,48,49,50,51,52,
+//             53,54,55,56,57,58,59,60,61,62,63,64,
+//             65,66,67,68,69,70,71,72,73,74,75,76,
+//             77,78,79,80,81,82,83,84,85,86,
+//             87,88,89,90,91
+//         ];
+
+//         this.loadGame();
+//     }
+
+//     showInformations() {
+//         document.querySelector('.player').innerHTML = JSON.parse(localStorage.getItem('Player'));
+//     }
+
+//     createBoats() {
+//         this.boat = document.createElement('img');
+//         this.boat.src = '/img/barco1.png';
+//         this.boat.classList.add('boat');
+
+//         document.querySelector('.area-game').append(this.boat);
+
+//         this.boat.addEventListener('click', function() {
+//             this.src = '/img/Explosion.png';
+//             let audio = document.querySelector('audio');
+//             audio.src = '/music/Som de explosão.mp3';
+//             audio.volume = 0.1;
+//             audio.play();
+//         })
+//     }
+
+//     time() {
+//         setInterval(() => {
+//             this.timer--
+//             document.querySelector('.timer').innerHTML = this.timer;
+//             if(this.timer == 0) {
+//                 this.timer = 30;
+//             }
+//         }, 1000);
+//     }
+
+//     randomBoats() {
+//         this.boats.forEach(b => {
+//             this.createBoats();
+//         });
+//     }
+
+//     checkWin() {
+        
+//     }
+
+//     loadGame() {
+//         this.showInformations();
+//         this.randomBoats();
+//         this.time();
+//         this.checkWin();
+//     }
+// }
+
+// let game = new Game();
 
 
 
